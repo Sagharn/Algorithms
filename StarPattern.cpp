0fe5d38aa9a96889e1f765ecf465c114b1fc5742
Sagharn <108654308+Sagharn@users.noreply.github.com>
2023-07-27 19:08:21 +0330

    Add files via upload
    Print Patterns

---
+++ StarPattern.cpp
@@ -0,0 +1,214 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n = 5;
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < n;j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 5;
+//	for (int i = 0;i < n;i++)
+//	{
+//		for (int j = 0;j < i;j++)
+//		{
+//			cout << " ";
+//		}
+//		for (int k = 0;k < n - i;k++)
+//		{
+//			cout << "*";
+//		}
+//		cout << "\n";
+//	}
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 5;
+//	for(int i =0;i<n;i++)
+//	{
+//		for(int j=0;j<n-i-1;j++)
+//		{
+//			cout<<" ";
+//		}
+//		for(int k=0;k<i+1;k++)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//	}
+//	return 0;
+//}
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 5;
+//	for (int i = 0;i < n;i++)
+//	{
+//		for (int j = 0;j < i + 1;j++)
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 5;
+//	for (int i = n;i >= 1;i--)
+//	{
+//		for (int j = 0;j < i;j++)
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int n = 5;
+//    for (int i=0; i<n; i++)
+//    {
+//        for (int j=0; j<n; j++)
+//        {
+//            if (i==0 || i==n-1 || j==0 || j==n-1)
+//            {
+//                cout << "*";
+//            }
+//            else
+//            {
+//                cout << " ";
+//            }
+//        }
+//        cout << endl;
+//    }
+//    return 0;
+//}
+
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n=3;
+//	for(int i=0;i<n;i++)
+//	{
+//		for(int j=0;j<n-i-1;j++)
+//		{
+//			cout<<" ";
+//		}
+//		for(int k=0;k<2*i+1;k++)
+//		{
+//			cout<<"*";
+//		}
+//		cout<<"\n";
+//	}
+//	return 0;
+//}
+
+
+
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int n = 3;
+//    for (int i = 1; i <= n; i++) 
+//    {
+//        for (int j = 1; j <= n - i; j++) 
+//        {
+//            cout << " ";
+//        }
+//        for (int k = 1; k <= 2 * i - 1; k++) 
+//        {
+//            cout << "*";
+//        }
+//        cout << endl; 
+//    }
+//    for (int i = n - 1; i >= 1; i--) 
+//    {
+//        for (int j = 1; j <= n - i; j++) 
+//        {
+//            cout << " ";
+//        }
+//        for (int k = 1; k <= 2 * i - 1; k++) 
+//        {
+//            cout << "*";
+//        }
+//        cout << endl; 
+//    }
+//    return 0;
+//}
+
+
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 5;
+//	for (int i = 1;i <= n;i++)
+//	{
+//		for (int j = 1;j <= n;j++)
+//		{
+//			if (i == 1 || i == n || j == 1 || j == n || i == j || j == n - i + 1)
+//			{
+//				cout << "*";
+//			}
+//			else
+//			{
+//				cout << " ";
+//			}
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
