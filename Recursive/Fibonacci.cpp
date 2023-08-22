a8a576aba69eae3d82aa90992a677eaaaac346db
Sagharn <108654308+Sagharn@users.noreply.github.com>
2023-08-22 23:17:07 +0330

    Fibonacci.cpp

--- Recursive/Fibonacci.cpp
+++ Recursive/Fibonacci.cpp
@@ -1,21 +1,72 @@
-//third recursive Question
+# O(n)
+def fib_linear(n, a=0, b=1):
+    if n == 0:
+        return a
+    if n == 1:
+        return b
+    return fib_linear(n - 1, b, a + b)
 
-#include <iostream>
-using namespace std;
-int fibonacci(int n) {
-    if (n == 0)
-        return 0;
-    else if (n == 1)
-        return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
 
-int main() {
-    int n;
-    cin >> n;
-    int fib_num = fibonacci(n);
-    cout<< fib_num << endl;
+n = int(input())
+nth_fib = fib_linear(n)
+print(nth_fib)
 
-    return 0;
-}
+print("--------------------------")
 
+
+# O(log n)
+
+def multiply(A, B):
+    n = len(A)
+    result = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                result[i][j] += A[i][k] * B[k][j]
+
+    return result
+
+
+def power(matrix, n):
+    size = len(matrix)
+    if n == 1:
+        return matrix
+
+    half_power = power(matrix, n // 2)
+    result = multiply(half_power, half_power)
+
+    if n % 2 == 1:
+        result = multiply(result, matrix)
+
+    return result
+
+
+def fib_log_n(n):
+    if n == 0:
+        return 0
+
+    base_matrix = [[1, 1], [1, 0]]
+    result_matrix = power(base_matrix, n - 1)
+
+    return result_matrix[0][0]
+
+
+n = int(input())
+nth_fib = fib_log_n(n)
+print(nth_fib)
+
+print("--------------------------")
+
+
+# O(2^n)
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+n = int(input())
+nth_fib = fibonacci(n)
+print(nth_fib)
