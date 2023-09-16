0862b582e348835502466b0f9ac70b10c3ddcbd6
Sagharn <108654308+Sagharn@users.noreply.github.com>
2023-09-16 18:36:12 +0330

    even array.py

--- Recursive/even array.py
+++ Recursive/even array.py
@@ -1,11 +1,7 @@
 def even(array, index):
     if index >= len(array):
         return 0
-    temp = 0
-    if array[index] % 2 == 0:
-        temp = 1
-    return temp + even(array, index + 1)
-
+    return int(array[index] % 2 == 0) + even(array, index + 1)
 
 arr = [6, 9, 8, 4, 1, 12]
 print(even(arr, 0))
