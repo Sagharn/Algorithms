d1362182dea63800cfbb87422adea31730a123dd
Sagharn <108654308+Sagharn@users.noreply.github.com>
2023-09-16 18:30:26 +0330

    Factorial.cpp

--- Recursive/Factorial.cpp
+++ Recursive/Factorial.cpp
@@ -1,14 +1,14 @@
-//second recursive 
-
 #include <iostream>
 using namespace std;
-int factorial(int n) {
-    if (n == 0 || n == 1)
+int factorial(int n) 
+{
+    if (n == 0)
         return 1;
     return n * factorial(n - 1);
 }
 
-int main() {
+int main() 
+{
     int num;
     cin >> num;
     int fact = factorial(num);
